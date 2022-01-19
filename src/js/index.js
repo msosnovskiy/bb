e9571a56458ddabf709e92cfd08a0cb693adb3d2
Maksim Sosnovskiy <maxim.sosnovsky@gmail.com>
2022-01-19 21:01:15 +0300

    added main pages

---
+++ src/js/index.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const textArray = ['you are beautiful', 'Ты Прекрасна', '你很美丽', 'ты прыгожая', 'du bist schön', 'ти прекрасна', 'tu es belle', 'eres hermoso', 'jesteś piękna', 'BEAUTI BAZA'];
+
+  const bannerText = document.querySelector('.company__title');
+
+
+
+  const menuContent = document.querySelector('.menu__content');
+
+  menuContent.onclick = function () {
+    document.querySelector('.menu__icon').classList.toggle('menu__icon_active');
+  }
+
+  let i = 0;
+  const slideText = (item, index, arrayLength) => {
+    //если фраза состоит более 2 слов - вставляем через перенос слова
+    if (item.split(' ').length > 1) {
+      let phrase = item.split(' ').join('<br>');
+      bannerText.innerHTML = phrase;
+
+      //Добавляем анимацию последнему слову
+      if (index === arrayLength - 1) {
+        bannerText.classList.add('company__title_glitch');
+        let spanFirst = document.createElement('span');
+        spanFirst.classList.add('company__title_span')
+        let spanSecond = document.createElement('span');
+        spanSecond.classList.add('company__title_span')
+        bannerText.prepend(spanFirst);
+        bannerText.prepend(spanSecond);
+        spanFirst.innerHTML = phrase;
+        spanSecond.innerHTML = phrase;
+
+      }
+    }
+
+    //если фраза состоит из одного слова то просто меняем текст
+    else bannerText.textContent = item;
+  }
+
+  let intervalID = setInterval(() => {
+    if (textArray[i] != undefined) {
+      slideText(textArray[i], i, textArray.length);
+      i++;
+    }
+    // Приостанавливаем выполнение функции если массив пуст
+    else {
+      clearInterval(intervalID);
+    }
+  }, 600);
+
+})
